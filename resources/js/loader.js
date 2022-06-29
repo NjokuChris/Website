d470a5b1e7f602b83088ac2b6a7a5a8fe8fd9031
Azeem <59115414+azeemade@users.noreply.github.com>
2022-06-29 15:39:55 +0100

    first commit

---
+++ resources/js/loader.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    console.log('ready');
+    $('.header_wraper').load('../components/common/header.html'); 
+    $('.navbar_wraper').load('../components/common/navbar.html');    
+    $('.footer_wraper').load('../components/common/footer.html');
+    
+});
